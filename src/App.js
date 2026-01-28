abce5704bb3349d790e16ea51a8583da309ec3ab
Gerardo Patricio Escamilla Carillo <patoescamillac@gmail.com>
2026-01-28 14:28:02 -0600

    contador click

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './MisEstilos.css';
 
 function App() {
+  //definir el estado
+  const[cuenta, setCuenta] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mi app en React esta funcionando</h1>
+        <p>Has hecho click {cuenta} veces</p>
+
+        <div>
+          <button onClick={ () => setCuenta(cuenta + 1) }>
+            Aumentar
+          </button> 
+          <button onClick={ () => setCuenta(cuenta - 1) }>
+            Disminuir
+          </button>
+        </div>
       </header>
     </div>
   );
